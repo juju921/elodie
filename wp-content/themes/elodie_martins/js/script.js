ed99bd48b44eb8453d79aad650313ff40173e979
juju921 <julien.garretb@gmail.com>
2016-11-22 17:20:38 +0100

    ajout menu responsive

--- wp-content/themes/elodie_martins/js/script.js
+++ wp-content/themes/elodie_martins/js/script.js
@@ -1,16 +1,14 @@
 //MENU STAY-TOP
-(function($){
-	$('#logo').hide();
-	var logoWidth = $('#logo').width;
+/*(function($){
 	var sticky_navigation_offset_top = $('#site-navigation').offset().top;
 	var sticky_navigation = function(){
 		var scroll_top = $(window).scrollTop();
 		if (scroll_top > sticky_navigation_offset_top) {
-			$('#site-navigation').css({ 'position': 'fixed', 'top':'20px', 'left':0.5 });
-			$('.logo').animate({width: 'show'});
+			$('#site-navigation').css({ 'position': 'fixed', 'top':0, 'left':0.5 });
+
 		} else {
 			$('#site-navigation').css({ 'position': 'relative' });
-			$('.logo').animate({width: 'hide'});
+
 		}
 	};
 
@@ -18,4 +16,47 @@
 	$(window).scroll(function() {
 		sticky_navigation();
 	});
-})(jQuery);
+})(jQuery);*/
+
+
+(function(w,d,undefined){
+	var el_html = d.documentElement,
+		el_body = d.getElementsByTagName('body')[0],
+		header = d.getElementById('site-navigation'),
+		menuIsStuck = function(triggerElement) {
+			var _scrollTop	= w.pageYOffset || el_body.scrollTop,
+				regexp		= /(nav\-is\-stuck)/i,
+				classFound	= el_html.className.match( regexp ),
+				navHeight	= header.offsetHeight,
+				bodyRect	= el_body.getBoundingClientRect(),
+				scrollValue	= triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight  : 800,
+				scrollValFix = classFound ? scrollValue : scrollValue + navHeight;
+
+			// if scroll down is 700 or more and nav-is-stuck class doesn't exist
+			if ( _scrollTop > scrollValFix && !classFound ) {
+				el_html.className = el_html.className + ' nav-is-stuck';
+				el_body.style.paddingTop = navHeight + 'px';
+			}
+
+			// if we are to high in the page and nav-is-stuck class exists
+			if ( _scrollTop <= 2 && classFound ) {
+				el_html.className = el_html.className.replace( regexp, '' );
+				el_body.style.paddingTop = '0px';
+			}
+		},
+		onScrolling = function() {
+			// this function fires menuIsStuck()…
+			menuIsStuck( d.getElementById('main') );
+			// and could do more stuff below
+		};
+
+
+	el_html.className = el_html.className + ' js';
+
+	// when you scroll, fire onScrolling() function
+	w.addEventListener('scroll', function(){
+		w.requestAnimationFrame( onScrolling );
+	});
+
+
+}(window, document,jQuery));
