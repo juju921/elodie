07cd51b2e9b6f1249ece7ad913236f768b3848ed
Julien Garret <julien.garretb@gmail.com>
2016-11-28 09:19:18 +0100

    menu page gallery

--- wp-content/themes/elodie_martins/js/script.js
+++ wp-content/themes/elodie_martins/js/script.js
@@ -19,44 +19,16 @@
 })(jQuery);*/
 
 
-(function(w,d,undefined){
-	var el_html = d.documentElement,
-		el_body = d.getElementsByTagName('body')[0],
-		header = d.getElementById('site-navigation'),
-		menuIsStuck = function(triggerElement) {
-			var _scrollTop	= w.pageYOffset || el_body.scrollTop,
-				regexp		= /(nav\-is\-stuck)/i,
-				classFound	= el_html.className.match( regexp ),
-				navHeight	= header.offsetHeight,
-				bodyRect	= el_body.getBoundingClientRect(),
-				scrollValue	= triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight  : 800,
-				scrollValFix = classFound ? scrollValue : scrollValue + navHeight;
+(function($){
 
-			// if scroll down is 700 or more and nav-is-stuck class doesn't exist
-			if ( _scrollTop > scrollValFix && !classFound ) {
-				el_html.className = el_html.className + ' nav-is-stuck';
-				el_body.style.paddingTop = navHeight + 'px';
-			}
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >= 290) {
 
-			// if we are to high in the page and nav-is-stuck class exists
-			if ( _scrollTop <= 2 && classFound ) {
-				el_html.className = el_html.className.replace( regexp, '' );
-				el_body.style.paddingTop = '0px';
-			}
-		},
-		onScrolling = function() {
-			// this function fires menuIsStuck()…
-			menuIsStuck( d.getElementById('main') );
-			// and could do more stuff below
-		};
-
-
-	el_html.className = el_html.className + ' js';
-
-	// when you scroll, fire onScrolling() function
-	w.addEventListener('scroll', function(){
-		w.requestAnimationFrame( onScrolling );
+			$('#site-navigation').addClass('stickytop');
+		}
+		else {
+			$('#site-navigation').removeClass('stickytop');
+		}
 	});
 
-
-}(window, document,jQuery));
+})(jQuery);
