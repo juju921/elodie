23c2e27847e2c363a363a0071420ea2b611ed388
Julien Garret <julien.garretb@gmail.com>
2016-11-30 14:35:10 +0100

    menu responsive

--- wp-content/themes/elodie_martins/header.php
+++ wp-content/themes/elodie_martins/header.php
@@ -46,6 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<a href="#" class="nav-toggle" aria-hidden="false">Menu</a>
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'elodie_martins' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
--- wp-content/themes/elodie_martins/js/script.js
+++ wp-content/themes/elodie_martins/js/script.js
@@ -1,34 +1,48 @@
 //MENU STAY-TOP
 /*(function($){
-	var sticky_navigation_offset_top = $('#site-navigation').offset().top;
-	var sticky_navigation = function(){
-		var scroll_top = $(window).scrollTop();
-		if (scroll_top > sticky_navigation_offset_top) {
-			$('#site-navigation').css({ 'position': 'fixed', 'top':0, 'left':0.5 });
+ var sticky_navigation_offset_top = $('#site-navigation').offset().top;
+ var sticky_navigation = function(){
+ var scroll_top = $(window).scrollTop();
+ if (scroll_top > sticky_navigation_offset_top) {
+ $('#site-navigation').css({ 'position': 'fixed', 'top':0, 'left':0.5 });
 
-		} else {
-			$('#site-navigation').css({ 'position': 'relative' });
+ } else {
+ $('#site-navigation').css({ 'position': 'relative' });
 
-		}
-	};
+ }
+ };
 
-	sticky_navigation();
-	$(window).scroll(function() {
-		sticky_navigation();
-	});
-})(jQuery);*/
+ sticky_navigation();
+ $(window).scroll(function() {
+ sticky_navigation();
+ });
+ })(jQuery);*/
 
 
-(function($){
+(function ($) {
 
-	$(window).scroll(function() {
-		if ($(this).scrollTop() >= 290) {
+	$ (window).scroll (function () {
+		if ($ (this).scrollTop () >= 290) {
 
-			$('#site-navigation').addClass('stickytop');
+			$ ('#site-navigation').addClass ('stickytop');
 		}
 		else {
-			$('#site-navigation').removeClass('stickytop');
+			$ ('#site-navigation').removeClass ('stickytop');
 		}
 	});
 
-})(jQuery);
+	$ ('.nav-toggle').click (function () {
+
+		if ($ (this).hasClass ('active')) {
+			$ (this).removeClass ('active');
+			$('.nav-menu').css("display", 'none');
+			$('#overlay').remove();
+
+		} else {
+			$ (this).addClass ('active');
+			$('.nav-menu').css("display", 'block');
+			$('<div id="overlay"> </div>').appendTo(document.body);
+		}
+	});
+
+}) (jQuery);
